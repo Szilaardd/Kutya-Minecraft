4995a0eea586ffb7287f0ed995cb90af2bf7551b
Bálint Szilárd <balint.szilard.2024i@bankitatabanya.hu>
2026-04-14 13:41:17 +0000

    save

--- start.sh
+++ start.sh
@@ -1,11 +1,20 @@
+#!/bin/bash
+
+# playit indítása háttérben
+./playit &
+
 while true
 do
-  java -Xms8G -Xmx8G -jar server.jar nogui
+  echo "Minecraft szerver indítása..."
   
+  java -Xms16G -Xmx16G -jar server.jar nogui
+  
+  echo "Szerver leállt, backup indul..."
+
   git add world/
   git commit -m "Auto backup $(date)" || echo "Nincs változás"
-
   git push
 
+  echo "Újraindítás 5 másodperc múlva..."
   sleep 5
 done
