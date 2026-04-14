346d56fa7bc605002c33f4ba2b271e62c397b0d6
Bálint Szilárd <balint.szilard.2024i@bankitatabanya.hu>
2026-04-14 15:30:41 +0000

    save

--- start.sh
+++ start.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-# playit indítása háttérben
-./playit &
+# Playit indítása háttérben (logolva)
+./playit > playit.log 2>&1 &
+echo "Playit elindítva (PID: $!)"
 
 while true
 do
   echo "Minecraft szerver indítása..."
-  
-  java -Xms16G -Xmx16G -jar server.jar nogui
-  
+
+  java -Xms8G -Xmx10G -XX:+UseG1GC -jar server.jar nogui
+
   echo "Szerver leállt, backup indul..."
 
+  # backup (csak ha van változás)
   git add world/
+
   git commit -m "Auto backup $(date)" || echo "Nincs változás"
-  git push
+  git push || echo "Git push sikertelen"
 
   echo "Újraindítás 5 másodperc múlva..."
   sleep 5
